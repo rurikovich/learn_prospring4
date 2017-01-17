dbdc37583f66d0703c845720fa66563456ee33af
rurik <urrast@yandex.ru>
2017-01-17 14:34:28 +0300

    getEnvironmentTest

--- src/main/java/org/learn/prospring4/Starter.java
+++ src/main/java/org/learn/prospring4/Starter.java
@@ -3,10 +3,15 @@ package org.learn.prospring4;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.io.Resource;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by rurik on 16.01.2017.
@@ -16,9 +21,23 @@ public class Starter {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
 
 //        getMessagesTest(ctx);
+//        getResourcesTest(ctx);
+        getEnvironmentTest(ctx);
 
-        getResourcesTest(ctx);
+    }
 
+    private static void getEnvironmentTest(ApplicationContext ctx) {
+        ConfigurableEnvironment env = (ConfigurableEnvironment) ctx.getEnvironment();
+
+        MutablePropertySources propertySources = env.getPropertySources();
+        Map<String, Object> аррМар = new HashMap<String, Object>();
+        аррМар.put("application.home", "application_home");
+        propertySources.addLast(new MapPropertySource("PROS PRING4 МАР", аррМар));
+        System.out.println("user.home: " + System.getProperty("user.home"));
+        System.out.println("JAVA_HOМE: " + System.getenv("JAVA_HOME"));
+        System.out.println("user.home: " + env.getProperty("user.home"));
+        System.out.println("JAVA_HOМE: " + env.getProperty("JAVA_HOМE"));
+        System.out.println("application.home: " + env.getProperty("application.home"));
     }
 
     private static void getMessagesTest(ApplicationContext ctx) {
