95aa55ec86a22cd2634e2a04e3d1277a2e994ca1
rurik <urrast@yandex.ru>
2017-01-25 18:17:55 +0300

    ListScriptEngines

---
+++ src/main/java/org/learn/prospring4/chapter14/ListScriptEngines.java
@@ -0,0 +1,22 @@
+package org.learn.prospring4.chapter14;
+
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+
+/**
+ * Created by rurik on 25.01.2017.
+ */
+public class ListScriptEngines {
+
+    public static void main(String[] args) {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        for (ScriptEngineFactory factory : manager.getEngineFactories()){
+            String engineName= factory.getEngineName();
+            String languageName = factory.getLanguageName();
+            String version = factory.getLanguageVersion();
+            System.out.println("Engine name: " + engineName + " Language: " +
+                    languageName + "version: " + version);
+
+        }
+    }
+}
